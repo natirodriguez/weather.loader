f927dd38ba3693aea8ac7503610445694caea9bf
natirodriguez <nati.stefania@gmail.com>
2025-06-16 19:04:42 -0300

    add logger

--- src/main/java/com/example/weather/loader/Application.java
+++ src/main/java/com/example/weather/loader/Application.java
@@ -1,14 +1,22 @@
 package com.example.weather.loader;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.example.weather.loader.component.WeatherLoaderComponent;
+
 @SpringBootApplication
 @EnableScheduling
 public class Application {
+    private static final Logger logger = LogManager.getLogger(Application.class);
 
 	public static void main(String[] args) {
+		
+		logger.info("Corriendo Weather Loader.");
+
 		SpringApplication.run(Application.class, args);
 	}
 
--- src/main/java/com/example/weather/loader/component/WeatherLoaderComponent.java
+++ src/main/java/com/example/weather/loader/component/WeatherLoaderComponent.java
@@ -7,9 +7,13 @@ import org.springframework.web.client.RestTemplate;
 import com.example.weather.loader.infraestructure.rest.dto.WeatherDataDTO;
 import com.example.weather.loader.service.WeatherLoaderService;
 import java.time.LocalDateTime;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Component
 public class WeatherLoaderComponent {
+    private static final Logger logger = LogManager.getLogger(WeatherLoaderComponent.class);
+
     private final String openWeatherApiKey = "a400bb918db70d65d96c4c27212bb92b"; 
     private final String city = "Quilmes,ar";
 
@@ -19,11 +23,13 @@ public class WeatherLoaderComponent {
     @Scheduled(fixedRate = 3600000) // Cada 60 segundos
     public void fetchAndSendWeather() {
         try {
-            RestTemplate restTemplate = new RestTemplate();
+        	logger.info("Ejecutando proceso...");
+
+        	RestTemplate restTemplate = new RestTemplate();
             String url = String.format(
                 "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric",
                 city, openWeatherApiKey);
-
+            
             String response = restTemplate.getForObject(url, String.class);
 
             com.fasterxml.jackson.databind.JsonNode node = new com.fasterxml.jackson.databind.ObjectMapper().readTree(response);
@@ -36,9 +42,11 @@ public class WeatherLoaderComponent {
             // Guardar en MongoDB
             weatherLoaderService.createWeatherData(weatherData);
             
+            logger.info("Temperatura actual guardada");
             System.out.println("Se guardo la temperatura actual para el día " + weatherData.getTimestamp());
         } catch (Exception e) {
+        	logger.error("Error encontrado: ", e);
             e.printStackTrace();
-        }
+        }        
     }
 }
